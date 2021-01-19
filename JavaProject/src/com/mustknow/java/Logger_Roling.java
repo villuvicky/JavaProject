2878fa1eacb186b2047ed092484c153219fa48a0
villu <villu@LAPTOP-LG2N16NC>
2021-01-19 11:52:06 +0530

    update

--- JavaProject/src/com/mustknow/java/Logger_Roling.java
+++ JavaProject/src/com/mustknow/java/Logger_Roling.java
@@ -1,20 +1,22 @@
 package com.mustknow.java;
 
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class Logger_Roling {
 
-	static Logger logger=Logger.getLogger(Logger_Roling.class);
+	static Logger logger=LogManager.getLogger(Logger_Roling.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//DOMCONFIGURATOR-for XML
-		PropertyConfigurator.configure("C:\\Users\\villu\\eclipse-workspace\\Java\\src\\mustKnow\\log4j.properties");
-logger.error("error");
-logger.debug("debug");
-logger.fatal("fatal");
-logger.info("info");
-logger.warn("warn");
+		//It takes default config if properties are not configured and prints only error and fatal
+		PropertyConfigurator.configure("Utilities//log4j.properties");
+		logger.error("error");
+		logger.debug("debug");
+		logger.fatal("fatal");
+		logger.info("info");
+		logger.warn("warn");
 	}
 
 }
